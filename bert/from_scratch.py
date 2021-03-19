1356a79462c20b7bb0dec459d97babc6b8ef8705
priscilaportela <priscila.portela.c@gmail.com>
2021-03-19 00:06:58 -0300

    train from scratch

--- bert/from_scratch.py
+++ bert/from_scratch.py
@@ -1,14 +1,16 @@
+#based on https://github.com/huggingface/blog/blob/master/notebooks/01_how_to_train.ipynb
+
 import torch
 from transformers import RobertaConfig
 from transformers import RobertaTokenizerFast
-from transformers import RobertaForMaskedLM
+from transformers import RobertaForMaskedLM, RobertaModel
 from transformers import LineByLineTextDataset
 from transformers import Trainer, TrainingArguments
 from transformers import DataCollatorForLanguageModeling
 
 #configs
 config = RobertaConfig(
-    vocab_size=52_000,
+    vocab_size=52000,
     max_position_embeddings=514,
     num_attention_heads=12,
     num_hidden_layers=6,
@@ -23,7 +25,7 @@ print('num params: {}'.format(model.num_parameters()))
 #training dataset
 dataset = LineByLineTextDataset(
     tokenizer=tokenizer,
-    file_path="???", #????
+    file_path="../results_file.txt", 
     block_size=128,
 )
 
@@ -31,14 +33,17 @@ data_collator = DataCollatorForLanguageModeling(
     tokenizer=tokenizer, mlm=True, mlm_probability=0.15
 )
 
+print('before trainer')
+
 #trainer
 training_args = TrainingArguments(
     output_dir="./latentbert",
     overwrite_output_dir=True,
-    num_train_epochs=1,
+    num_train_epochs=100,
     per_gpu_train_batch_size=64,
-    save_steps=10_000,
+    save_steps=10000,
     save_total_limit=2,
+    prediction_loss_only=True,
 )
 
 trainer = Trainer(
@@ -46,7 +51,6 @@ trainer = Trainer(
     args=training_args,
     data_collator=data_collator,
     train_dataset=dataset,
-    prediction_loss_only=True,
 )
 
 trainer.train()
