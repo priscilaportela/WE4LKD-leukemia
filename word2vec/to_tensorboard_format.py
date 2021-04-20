9a4d3c0433e5b2b288b7ee7893957757dc829ead
priscilaportela <priscila.portela.c@gmail.com>
2021-04-20 13:39:05 -0300

    fix function

--- word2vec/to_tensorboard_format.py
+++ word2vec/to_tensorboard_format.py
@@ -9,11 +9,11 @@ for idx, key in enumerate(model.wv.vocab):
     metadata.append(key)
     word_vectors.append(model.wv[key].tolist())
 
-with open("metadata.tsv", 'w') as output:
+with open("metadata.tsv", 'w', encoding='utf-8') as output:
     for m in metadata:
         output.write(str(m) + '\n')
 
-with open("vectors.tsv", 'w') as output:
+with open("vectors.tsv", 'w', encoding='utf-8') as output:
     for vw in word_vectors:
         vw = map(str, vw)
         output.write('\t'.join(vw) + '\n')
