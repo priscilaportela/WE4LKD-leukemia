ac821582553f6ea0a9a4be4ffd197343e64b8a91
priscilaportela <priscila.portela.c@gmail.com>
2021-04-19 22:16:59 -0300

    generate data to vizualise on projector

---
+++ word2vec/to_tensorboard_format.py
@@ -0,0 +1,22 @@
+from gensim.models import Word2Vec
+import numpy as np
+
+model = Word2Vec.load('model.bin')
+
+metadata = []
+word_vectors = []
+for idx, key in enumerate(model.wv.vocab):
+    metadata.append(key)
+    word_vectors.append(model.wv[key].tolist())
+
+with open("metadata.tsv", 'w') as output:
+    for m in metadata:
+        output.write(str(m) + '\n')
+
+with open("vectors.tsv", 'w') as output:
+    for vw in word_vectors:
+        vw = map(str, vw)
+        output.write('\t'.join(vw) + '\n')
+
+
+
