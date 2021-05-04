34d78be6f2fd38e1049895e2d8cf98ef80d4bcf2
priscilaportela <priscila.portela.c@gmail.com>
2021-05-03 21:16:22 -0300

    remove common word in english

--- to_tensorboard_format.py
+++ to_tensorboard_format.py
@@ -2,12 +2,16 @@ from gensim.models import Word2Vec
 import numpy as np
 import sys
 import os
+from get_n_common_words_english import get_most_common
 
 #create folder if not exists
 os.makedirs('tensorboard_inputs', exist_ok=True) 
 
 #parser
 embedding = sys.argv[1]
+
+#get most common words to remove from projector file
+list_common_words = get_most_common(n=10000)
 
 if embedding == 'word2vec':
     model = Word2Vec.load('./word2vec/model.bin')
@@ -18,16 +22,26 @@ if embedding == 'word2vec':
         metadata.append(key)
         word_vectors.append(model.wv[key].tolist())
 
-    with open("metadata_w2v.tsv", 'w', encoding='utf-8') as output:
-    for m in metadata:
-        output.write(str(m) + '\n')
+    index_to_remove_list = []
+    for l in list_common_words:
+        try:
+            index_to_remove_list.append(metadata.index(l))
+        except:
+            pass
 
-    with open("vectors_w2v.tsv", 'w', encoding='utf-8') as output:
+    metadata = [i for j, i in enumerate(metadata) if j not in index_to_remove_list]
+    word_vectors = [i for j, i in enumerate(word_vectors) if j not in index_to_remove_list]
+
+    with open("./tensorboard_inputs/metadata_w2v.tsv", 'w', encoding='utf-8') as output:
+        for m in metadata:
+            output.write(str(m) + '\n')
+
+    with open("./tensorboard_inputs/vectors_w2v.tsv", 'w', encoding='utf-8') as output:
         for vw in word_vectors:
             vw = map(str, vw)
             output.write('\t'.join(vw) + '\n')
 
-if embedding == 'glove'
+if embedding == 'glove':
     vector_list = [v.strip() for v in open("./glove/vectors.txt", encoding="utf-8")]
 
     metadata = []
@@ -36,11 +50,21 @@ if embedding == 'glove'
         metadata.append(v.split(' ')[0])
         word_vectors.append(v.split(' ')[1:])
 
-    with open("metadata_glove.tsv", 'w') as output:
-        for m in metadata:
-        output.write(str(m) + '\n')
+    index_to_remove_list = []
+    for l in list_common_words:
+        try:
+            index_to_remove_list.append(metadata.index(l))
+        except:
+            pass
 
-    with open("vectors_glove.tsv", 'w') as output:
+    metadata = [i for j, i in enumerate(metadata) if j not in index_to_remove_list]
+    word_vectors = [i for j, i in enumerate(word_vectors) if j not in index_to_remove_list]
+
+    with open("./tensorboard_inputs/metadata_glove.tsv", 'w') as output:
+        for m in metadata:
+            output.write(str(m) + '\n')
+
+    with open("./tensorboard_inputs/vectors_glove.tsv", 'w') as output:
         for vw in word_vectors:
             vw = map(str, vw)
             output.write('\t'.join(vw) + '\n')
