142c0d46054a4d52af653c78020618789c57587b
priscilaportela <priscila.portela.c@gmail.com>
2021-05-04 16:13:46 -0300

    get vectors from bert

--- bert/get_vector.py
+++ bert/get_vector.py
@@ -1,4 +1,5 @@
 import torch
+import pandas as pd
 from transformers import RobertaConfig
 from transformers import RobertaTokenizerFast
 from transformers import RobertaForMaskedLM, RobertaModel
@@ -9,14 +10,33 @@ from pathlib import Path
 from tokenizers import ByteLevelBPETokenizer
 
 
-def get_word_vector(word):
+df = pd.read_csv('metadata_w2v.tsv', header=None)
+word_list = list(df[0])
 
-    tokenizer = RobertaTokenizerFast.from_pretrained('./tentativa')
-    model = RobertaModel.from_pretrained('./tentativa')
+word_vectors = []
+words = []
 
-    input_ids = torch.tensor(tokenizer.encode("leukemia")).unsqueeze(0)  # Batch size 1
+tokenizer = RobertaTokenizerFast.from_pretrained('./roberta_we4lkd')
+model = RobertaModel.from_pretrained('./roberta_we4lkd')
 
-    outputs = model(input_ids)
-    last_hidden_states = outputs[0]  # The last hidden-state is the first element of the output tuple
-    last_layer = last_hidden_states[-1]
-    return last_layer[-1]
+for w in word_list:
+    try:
+        input_ids = torch.tensor(tokenizer.encode(w)).unsqueeze(0)  # Batch size 1
+
+        outputs = model(input_ids)
+        last_hidden_states = outputs[0]  # The last hidden-state is the first element of the output tuple
+        last_layer = last_hidden_states[-1]
+        words.append(w)
+        word_vectors.append(last_layer[-1].tolist())
+        print(w)
+    except:
+        pass
+
+with open("./vectors_roberta.tsv", 'w', encoding='utf-8') as output:
+    for vw in word_vectors:
+        vw = map(str, vw)
+        output.write('\t'.join(vw) + '\n')
+
+with open("./metadata_roberta.tsv", 'w', encoding='utf-8') as output:
+    for m in words:
+        output.write(str(m) + '\n')
--- bert/train_distilbert_from_scratch.py
+++ bert/train_distilbert_from_scratch.py
@@ -1,9 +1,9 @@
 #based on https://github.com/huggingface/blog/blob/master/notebooks/01_how_to_train.ipynb
 
 import torch
-from transformers import RobertaConfig
-from transformers import RobertaTokenizerFast
-from transformers import RobertaForMaskedLM, RobertaModel
+from transformers import DistilBertConfig
+from transformers import DistilBertTokenizerFast
+from transformers import DistilBertForMaskedLM, DistilBertModel
 from transformers import LineByLineTextDataset
 from transformers import Trainer, TrainingArguments
 from transformers import DataCollatorForLanguageModeling
@@ -11,6 +11,7 @@ from transformers import DataCollatorForLanguageModeling
 from pathlib import Path
 
 from tokenizers import ByteLevelBPETokenizer
+
 import os
 
 paths = ["../results_file_clean.txt"]
@@ -19,7 +20,7 @@ paths = ["../results_file_clean.txt"]
 tokenizer = ByteLevelBPETokenizer()
 
 # Customize training
-tokenizer.train(files=paths, vocab_size=52_000, min_frequency=2, special_tokens=[
+tokenizer.train(files=paths, vocab_size=52000, min_frequency=2, special_tokens=[
     "<s>",
     "<pad>",
     "</s>",
@@ -27,45 +28,44 @@ tokenizer.train(files=paths, vocab_size=52_000, min_frequency=2, special_tokens=
     "<mask>",
 ])
 
-os.makedirs('tentativa', exist_ok=True) 
-tokenizer.save_model("tentativa")
+os.makedirs('distilbert_we4lkd', exist_ok=True) 
+tokenizer.save_model("distilbert_we4lkd")
 
 from tokenizers.implementations import ByteLevelBPETokenizer
 from tokenizers.processors import BertProcessing
 
 
 tokenizer = ByteLevelBPETokenizer(
-    "./tentativa/vocab.json",
-    "./tentativa/merges.txt",
+    "./distilbert_we4lkd/vocab.json",
+    "./distilbert_we4lkd/merges.txt",
 )
 
 tokenizer._tokenizer.post_processor = BertProcessing(
     ("</s>", tokenizer.token_to_id("</s>")),
     ("<s>", tokenizer.token_to_id("<s>")),
 )
-tokenizer.enable_truncation(max_length=512)
+
+tokenizer.enable_truncation(max_length=200)
 
 
 import torch
 print(torch.cuda.is_available())
 
-from transformers import RobertaConfig
+from transformers import DistilBertConfig
 
-config = RobertaConfig(
-    vocab_size=52_000,
+config = DistilBertConfig(
+    vocab_size=52000,
     max_position_embeddings=514,
-    num_attention_heads=12,
-    num_hidden_layers=6,
     type_vocab_size=1,
 )
 
-from transformers import RobertaTokenizerFast
+from transformers import DistilBertTokenizerFast
 
-tokenizer = RobertaTokenizerFast.from_pretrained("./tentativa", max_len=512)
+tokenizer = DistilBertTokenizerFast.from_pretrained("./distilbert_we4lkd", max_len=512)
 
-from transformers import RobertaForMaskedLM
+from transformers import DistilBertForMaskedLM
 
-model = RobertaForMaskedLM(config=config)
+model = DistilBertForMaskedLM(config=config)
 
 model.num_parameters()
 
@@ -86,11 +86,11 @@ data_collator = DataCollatorForLanguageModeling(
 from transformers import Trainer, TrainingArguments
 
 training_args = TrainingArguments(
-    output_dir="./tentativa",
+    output_dir="./distilbert_we4lkd",
     overwrite_output_dir=True,
-    num_train_epochs=1,
-    per_gpu_train_batch_size=64,
-    save_steps=10_000,
+    num_train_epochs=4,
+    per_gpu_train_batch_size=1,
+    save_steps=10000,
     save_total_limit=2,
     prediction_loss_only=True,
 )
@@ -105,7 +105,6 @@ trainer = Trainer(
 
 trainer.train()
 
-trainer.save_model("./tentativa")
-
+trainer.save_model("./distilbert_we4lkd")
 
 
