bd933f15a4b6aeb7a89f87ad204fada92cb3c739
priscilaportela <priscila.portela.c@gmail.com>
2021-05-18 22:41:25 -0300

    fix merge.txt

--- merge_txt.py
+++ merge_txt.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 
-filenames = [str(x) for x in Path("./results/").glob("**/*.txt")]
+filenames = [str(x) for x in Path('./results/').glob('**/*.txt')]
 
-with open('results_file.txt', 'w', encoding="utf-8") as outfile:
-    for fname in filenames:
-        with open(fname, encoding="utf-8") as infile:
-            for line in infile:
-                outfile.write(line)
+abstract_list = []
+for fname in filenames:
+    with open(fname, encoding='utf-8') as infile:
+        for line in infile:
+            abstract_list.append(line)
+
+with open('results_file.txt','w', encoding='utf-8') as f:
+    for row in abstract_list:
+        f.write(repr(row)+'\n')
