a6a71eeca64862740ab733a68bc23f24d2e0f9cc
priscilaportela <priscila.portela.c@gmail.com>
2021-06-29 10:56:44 -0300

    test file

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -12,8 +12,8 @@ number_option = st.selectbox(
      'Escolha um numero de vizinhos:',
      (5, 10, 20, 50))
 
-df_metadata = pd.read_csv('../tensorboard_inputs/metadata_w2v.tsv', sep='\t', header=None)
-df_vectores = pd.read_csv('../tensorboard_inputs/vectors_w2v.tsv', sep='\t', header=None)
+df_metadata = pd.read_csv('metadata_w2v.tsv', sep='\t', header=None)
+df_vectores = pd.read_csv('vectors_w2v.tsv', sep='\t', header=None)
 
 print(df_metadata.head())
 
