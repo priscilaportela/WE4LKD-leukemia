abe71f71a5954d63861d89b562b692c713550ffa
priscilaportela <priscila.portela.c@gmail.com>
2021-06-29 12:28:59 -0300

    add correct path

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -12,10 +12,10 @@ number_option = st.selectbox(
      'Escolha um numero de vizinhos:',
      (5, 10, 20, 50))
 
-df_metadata = pd.read_csv('https://s3.console.aws.amazon.com/s3/object/we4lkd?region=us-east-1&prefix=metadata_w2v.tsv', 
+df_metadata = pd.read_csv('s3://we4lkd/metadata_w2v.tsv', 
                           sep='\t', 
                           header=None)
-df_vectors = pd.read_csv('https://s3.console.aws.amazon.com/s3/object/we4lkd?region=us-east-1&prefix=vectors_w2v.tsv', sep='\t', header=None)
+df_vectors = pd.read_csv('s3://we4lkd/vectors_w2v.tsv', sep='\t', header=None)
 
 st.write(df_metadata.head())
 st.write(df_vectors.head())
