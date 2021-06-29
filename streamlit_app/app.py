9dd6af4d7ec49f0552f114004be5fc1d9f786d7f
priscilaportela <priscila.portela.c@gmail.com>
2021-06-29 11:39:05 -0300

    show data

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -15,9 +15,10 @@ number_option = st.selectbox(
 df_metadata = pd.read_csv('https://s3.console.aws.amazon.com/s3/object/we4lkd?region=us-east-1&prefix=metadata_w2v.tsv', 
                           sep='\t', 
                           header=None)
-df_vectores = pd.read_csv('vectors_w2v.tsv', sep='\t', header=None)
+df_vectors = pd.read_csv('https://s3.console.aws.amazon.com/s3/object/we4lkd?region=us-east-1&prefix=vectors_w2v.tsv', sep='\t', header=None)
 
-print(df_metadata.head())
+st.write(df_metadata.head())
+st.write(df_vectors.head())
 
 st.markdown('## Paravras mais proximas')
-chosen_word_vector = df.c1[a.c1 == 8].index.tolist()
+#chosen_word_vector = df.c1[a.c1 == 8].index.tolist()
